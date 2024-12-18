ddae9e2d1ef70cf3639c1cd266fb6c6057a732c7
nithin0892 <nikky.kollu@gmail.com>
2024-12-18 10:27:21 -0500

    Bkp

--- gluetransform_private.py
+++ gluetransform_private.py
@@ -43,7 +43,8 @@ permission_policy = {
                 "athena:*",
                 "logs:CreateLogGroup",
                 "logs:CreateLogStream",
-                "logs:PutLogEvents"
+                "logs:PutLogEvents",
+                "cloudwatch:PutMetricData"
             ],
             "Resource": "*"
         }
